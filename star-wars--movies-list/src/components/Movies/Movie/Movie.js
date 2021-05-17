504eeb9664ad13aa4114b3a24748b8ed6ce12f7e
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-17 15:45:23 +0200

    Imported styles

--- star-wars--movies-list/src/components/Movies/Movie/Movie.js
+++ star-wars--movies-list/src/components/Movies/Movie/Movie.js
@@ -1,6 +1,8 @@
+import classes from './Movie.module.css';
+
 const Movie = props => {
 	return (
-		<li>
+		<li className={classes.movie}>
 			<h2>{props.title}</h2>
 			<h3>{props.releaseDate}</h3>
 			<p>{props.openingText}</p>
