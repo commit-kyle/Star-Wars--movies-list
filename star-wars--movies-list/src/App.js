3aa6ac5907700673b3ad60f920e1337b29e9a7ed
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-27 18:20:43 +0200

    Configured 'fetch API' logic to retrieve movie data from database

--- star-wars--movies-list/src/App.js
+++ star-wars--movies-list/src/App.js
@@ -1,17 +1,37 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import './App.css';
 
 import MovieList from './components/Movies/MovieList/MovieList';
 
 function App() {
+	const [movies, setMovies] = useState([]);
+
+	const baseURL = 'https://swapi.dev/api/films/';
+
+	const fetchMoviesHandler = async () => {
+
+		const response = await fetch(baseURL);
+		const data = await response.json();
+
+		const transformedMovies = data.results.map(movie => {
+			return {
+				id: movie.episode_id,
+				title: movie.title,
+				openingText: movie.opening_crawl,
+				releaseDate: movie.release_date,
+			};
+		});
+
+		setMovies(transformedMovies);
+	};
+
 	return (
 		<Fragment>
 			<section>
-				<button>Fetch Movies</button>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
-			<section>
-				<MovieList movies={[]} />
+			<section><MovieList movies={movies}/>
 			</section>
 		</Fragment>
 	);
