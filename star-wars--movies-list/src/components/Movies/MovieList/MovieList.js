ef657e3a4d5f8c8cf907c3177aeb2d3f929a3156
Kyle Buttress <kylebuttress11@gmail.com>
2021-05-27 19:41:08 +0200

    Reconfigured and optimised JSX to render content to the DOM correctly

--- star-wars--movies-list/src/components/Movies/MovieList/MovieList.js
+++ star-wars--movies-list/src/components/Movies/MovieList/MovieList.js
@@ -4,16 +4,18 @@ import Movie from '../Movie/Movie';
 
 const MovieList = props => {
 	return (
-		<ul className={classes['movies-list']}>
-			{props.movies.map(movie => (
-				<Movie
-					key={movie.id}
-					title={movie.title}
-					releaseDate={movie.release}
-					openingText={movie.openingText}
-				/>
-			))}
-		</ul>
+		<section>
+			<ul className={classes['movies-list']}>
+				{props.movies.map(movie => (
+					<Movie
+						key={movie.id}
+						title={movie.title}
+						releaseDate={movie.release}
+						openingText={movie.openingText}
+					/>
+				))}
+			</ul>
+		</section>
 	);
 };
 
